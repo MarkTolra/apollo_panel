1f2caaf5aba57ca9c4ac5bd05cb09c5c7e198fb1
Mark Tolra <mtolra@gmail.com>
2019-10-05 16:49:42 +1300

    battery monitor function

--- apollo_panel/src/main.cpp
+++ apollo_panel/src/main.cpp
@@ -1,9 +1,33 @@
+/*
+Apollo inspired "control panel" master board code. The slave handles the LCD
+
+Created: 5/10/19
+*/
+
 #include <Arduino.h>
+#include <Wire.h>
+
+const int battPin = A0;
+
+uint32_t battMonitor();
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Start up");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println(battMonitor());
+  delay(1000);
+}
+
+uint32_t battMonitor()
+{
+  // reads and returns the voltage for battery monitoring'''
+
+  int sensorValue = analogRead(battPin);
+  float voltage = sensorValue * (5.0 /1023.0) * 2;  // convert the value to a true value
+  // Serial.println(voltage);
+
+  return voltage;
 }
